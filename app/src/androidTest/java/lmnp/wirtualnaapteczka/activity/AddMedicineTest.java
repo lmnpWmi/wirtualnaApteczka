e24d8e237cc36bbf54e02aca945c48e727c8b4bd
Jowita <jowita.mielnicka@gmail.com>
2017-07-02 11:29:13 +0200

    WA-22 | add UI test - add medicine

---
+++ app/src/androidTest/java/lmnp/wirtualnaapteczka/activity/AddMedicineTest.java
@@ -0,0 +1,164 @@
+package lmnp.wirtualnaapteczka.activity;
+
+
+import android.support.test.espresso.ViewInteraction;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import com.schibsted.spain.barista.cleardata.ClearPreferencesRule;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import lmnp.wirtualnaapteczka.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.pressImeActionButton;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class AddMedicineTest {
+
+    @Rule
+    public ActivityTestRule<IntroActivity> mActivityTestRule = new ActivityTestRule<>(IntroActivity.class);
+
+    @Rule public ClearPreferencesRule clearPreferencesRule = new ClearPreferencesRule();
+
+    @Test
+    public void addMedicineTest() {
+        ViewInteraction appCompatButton = onView(
+                allOf(withId(R.id.skipBtn), withText("Pomiń"), isDisplayed()));
+        appCompatButton.perform(click());
+
+        ViewInteraction floatingActionButton = onView(
+                allOf(withId(R.id.addMedicineFAB), isDisplayed()));
+        floatingActionButton.perform(click());
+
+        ViewInteraction appCompatEditText = onView(
+                allOf(withId(R.id.medicineName), isDisplayed()));
+        appCompatEditText.perform(click());
+
+        ViewInteraction appCompatEditText2 = onView(
+                allOf(withId(R.id.medicineName), isDisplayed()));
+        appCompatEditText2.perform(replaceText("witamina C"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText3 = onView(
+                allOf(withId(R.id.medicineName), withText("witamina C"), isDisplayed()));
+        appCompatEditText3.perform(pressImeActionButton());
+
+        ViewInteraction appCompatEditText4 = onView(
+                allOf(withId(R.id.medicineCategory), isDisplayed()));
+        appCompatEditText4.perform(replaceText("witaminy"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText5 = onView(
+                allOf(withId(R.id.medicineCategory), withText("witaminy"), isDisplayed()));
+        appCompatEditText5.perform(pressImeActionButton());
+
+        ViewInteraction appCompatSpinner = onView(
+                allOf(withId(R.id.medicineTypeEnum), isDisplayed()));
+        appCompatSpinner.perform(click());
+
+        ViewInteraction appCompatTextView = onView(
+                allOf(withId(R.id.dropdownElement), withText("Tabletka"), isDisplayed()));
+        appCompatTextView.perform(click());
+
+        ViewInteraction appCompatEditText6 = onView(
+                allOf(withId(R.id.medicineAmount), isDisplayed()));
+        appCompatEditText6.perform(replaceText("15"), closeSoftKeyboard());
+
+        ViewInteraction appCompatEditText7 = onView(
+                allOf(withId(R.id.medicineAmount), withText("15"), isDisplayed()));
+        appCompatEditText7.perform(pressImeActionButton());
+
+        ViewInteraction appCompatTextView2 = onView(
+                allOf(withId(R.id.medicineDueDate), isDisplayed()));
+        appCompatTextView2.perform(click());
+
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withClassName(is("android.support.v7.widget.AppCompatImageButton")), withContentDescription("Następny miesiąc"),
+                        withParent(allOf(withClassName(is("android.widget.DayPickerView")),
+                                withParent(withClassName(is("com.android.internal.widget.DialogViewAnimator"))))),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction appCompatButton2 = onView(
+                allOf(withId(android.R.id.button1), withText("OK")));
+        appCompatButton2.perform(scrollTo(), click());
+
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.action_add), withText("Dodaj"), withContentDescription("Dodaj"), isDisplayed()));
+        actionMenuItemView.perform(click());
+
+        ViewInteraction textView = onView(
+                allOf(withId(R.id.name), withText("witamina C"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                0),
+                        isDisplayed()));
+        textView.check(matches(withText("witamina C")));
+
+        ViewInteraction textView2 = onView(
+                allOf(withId(R.id.type), withText("Tabletka"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                1),
+                        isDisplayed()));
+        textView2.check(matches(withText("Tabletka")));
+
+        ViewInteraction textView3 = onView(
+                allOf(withId(R.id.amount), withText("Ilość: 15"),
+                        childAtPosition(
+                                childAtPosition(
+                                        IsInstanceOf.<View>instanceOf(android.widget.LinearLayout.class),
+                                        0),
+                                2),
+                        isDisplayed()));
+        textView3.check(matches(withText("Ilość: 15")));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
+    }
+}
