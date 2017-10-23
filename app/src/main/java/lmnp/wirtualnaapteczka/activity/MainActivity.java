3069db9ce0a50e31856cf8b13101aa7b4713f6bd
Sebastian Nowak <sebnow2@st.amu.edu.pl>
2017-10-23 21:12:49 +0200

    Deleted old project files

---
+++ app/src/main/java/lmnp/wirtualnaapteczka/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package lmnp.wirtualnaapteczka.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import lmnp.wirtualnaapteczka.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
