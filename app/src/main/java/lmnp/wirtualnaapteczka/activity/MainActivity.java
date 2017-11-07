ba243a1dcf7ab368de663b988d79ada6dfdb0055
Jowita <jowita.mielnicka@gmail.com>
2017-11-07 23:06:11 +0100

    WA-30| add menu main (and one change in MainActivity)

--- app/src/main/java/lmnp/wirtualnaapteczka/activity/MainActivity.java
+++ app/src/main/java/lmnp/wirtualnaapteczka/activity/MainActivity.java
@@ -2,6 +2,8 @@ package lmnp.wirtualnaapteczka.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
 import lmnp.wirtualnaapteczka.R;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,4 +13,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
 }
