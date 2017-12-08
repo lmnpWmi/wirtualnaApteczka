e415d86ce6baebcbc5c41fcaa4476fe1c390ff81
Jowita <jowita.mielnicka@gmail.com>
2017-12-08 13:05:33 +0100

    WA-35 Add family layout

---
+++ app/src/main/java/lmnp/wirtualnaapteczka/activity/FamilyActivity.java
@@ -0,0 +1,16 @@
+package lmnp.wirtualnaapteczka.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lmnp.wirtualnaapteczka.R;
+
+public class FamilyActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_family);
+    }
+}
