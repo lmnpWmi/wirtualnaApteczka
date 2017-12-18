1e5eaff49b47ed60cd01fdd1b13b49346880341f
Jowita <jowita.mielnicka@gmail.com>
2017-12-18 22:34:49 +0100

    WA-36 | add name to top panel (reminder password, registration, log in..)

---
+++ app/src/main/java/lmnp/wirtualnaapteczka/activity/PasswordReminderActivity.java
@@ -0,0 +1,19 @@
+package lmnp.wirtualnaapteczka.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import lmnp.wirtualnaapteczka.R;
+
+/**
+ * Created by Jowita on 2017-12-18.
+ */
+
+public class PasswordReminderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_password_reminder);
+    }
+}
