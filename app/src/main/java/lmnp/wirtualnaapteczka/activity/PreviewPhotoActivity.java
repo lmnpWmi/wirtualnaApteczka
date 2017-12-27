4cb2b5c504704f0351548426154b443219f9f6c8
Jowita <jowita.mielnicka@gmail.com>
2017-12-27 21:54:55 +0100

    new layout - Preview Photo Activity

---
+++ app/src/main/java/lmnp/wirtualnaapteczka/activity/PreviewPhotoActivity.java
@@ -0,0 +1,14 @@
+package lmnp.wirtualnaapteczka.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import lmnp.wirtualnaapteczka.R;
+
+public class PreviewPhotoActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview_photo);
+    }
+}
