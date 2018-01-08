0f18872af87d0929d8f94fcdf01f5b2a3b03ce08
Sebastian Nowak <sebnow2@st.amu.edu.pl>
2018-01-08 22:06:30 +0100

    Corrected font in registration edit texts for passwords

--- app/src/main/java/lmnp/wirtualnaapteczka/activity/RegistrationActivity.java
+++ app/src/main/java/lmnp/wirtualnaapteczka/activity/RegistrationActivity.java
@@ -1,18 +1,22 @@
 package lmnp.wirtualnaapteczka.activity;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.PasswordTransformationMethod;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import lmnp.wirtualnaapteczka.R;
-import lmnp.wirtualnaapteczka.data.dto.UserRegistrationTO;
 import lmnp.wirtualnaapteczka.listeners.loginactivity.RegulationsOnClickListener;
 import lmnp.wirtualnaapteczka.listeners.registrationactivity.RegisterNewUserOnClickListener;
 
 public class RegistrationActivity extends AppCompatActivity {
     private Button registerNewUserBtn;
     private TextView registerRegulationsBtn;
+
+    private EditText passwordEditText;
+    private EditText repeatPasswordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +28,13 @@ public class RegistrationActivity extends AppCompatActivity {
 
         registerNewUserBtn = (Button) findViewById(R.id.register_new_user_btn);
         registerNewUserBtn.setOnClickListener(new RegisterNewUserOnClickListener());
+
+        passwordEditText = (EditText) findViewById(R.id.register_password_text);
+        passwordEditText.setTypeface(Typeface.DEFAULT);
+        passwordEditText.setTransformationMethod(new PasswordTransformationMethod());
+
+        repeatPasswordEditText = (EditText) findViewById(R.id.register_password_repeat_text);
+        repeatPasswordEditText.setTypeface(Typeface.DEFAULT);
+        repeatPasswordEditText.setTransformationMethod(new PasswordTransformationMethod());
     }
 }
